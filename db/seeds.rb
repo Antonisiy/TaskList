5a8947ab6fdd27e994de84b1a4b35c24d6bde1d5
Antonisiy <meanto@yandex.ru>
2018-07-05 23:14:46 +0300

    Part two_one

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,21 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+
+seed_file = Rails.root.join('db', 'seeds', 'seeds.yml')
+@config = HashWithIndifferentAccess.new(YAML.load(File.read((seed_file))))
+	@config[:projects].each do |values|
+		#puts values.size
+			@title = values[:title]
+			@todos = values[:todos]
+			@projects = Project.create! title: @title
+			@temp_todo = Todo.create!(@todos)
+			@projects.todos = @temp_todo
+	end
+
+
+
+#seed_file = Rails.root.join('db', 'seeds', 'seeds.yml')
+#config = HashWithIndifferentAccess.new(YAML::load_file(seed_file))
+#Project.create(config["projects"])
+#Todo.create(config["todos"])
