39ee87c66c75673645fb03b2679ba480ed91de98
Antonisiy <meanto@yandex.ru>
2018-07-06 00:33:59 +0300

    Fix bug 2

--- app/controllers/todos_controller.rb
+++ app/controllers/todos_controller.rb
@@ -29,7 +29,7 @@ class TodosController < ApplicationController
 
     respond_to do |format|
       if @todo.save
-        format.html { redirect_to @todo, notice: 'Todo was successfully created.' }
+        format.html { redirect_to @todo, notice: 'Задача успешно добавлена!' }
         format.json { render :show, status: :created, location: @todo }
       else
         format.html { render :new }
