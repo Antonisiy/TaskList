df7b346f0449e31dcf5cebe388d6409c6675e57b
Antonisiy <meanto@yandex.ru>
2018-07-11 02:54:24 +0300

    Fix bug

--- config/application.rb
+++ config/application.rb
@@ -10,7 +10,7 @@ module TaskList
   class Application < Rails::Application
     # Initialize configuration defaults for originally generated Rails version.
     config.load_defaults 5.2
-    config.assets.precompile « /\.(?:svg|eot|woff|ttf)\z/
+    config.assets.precompile << /\.(?:svg|eot|woff|ttf)\z/
 	config.assets.paths << Rails.root.join('app', 'assets', 'fonts')
 	config.assets.initialize_on_precompile = false
     # Settings in config/environments/* take precedence over those specified here.
